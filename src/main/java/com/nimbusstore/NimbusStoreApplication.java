66beb73af40a2be678ce12469aeb82cda31d0ba4
Romain <65140547+romainyvernes@users.noreply.github.com>
2025-08-06 15:42:49 -0700

    Add files generated with Spring Initializr

---
+++ src/main/java/com/nimbusstore/NimbusStoreApplication.java
@@ -0,0 +1,13 @@
+package com.nimbusstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimbusStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimbusStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nimbusstore/NimbusStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimbusstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimbusStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
